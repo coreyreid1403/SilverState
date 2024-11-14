fe6c25b8d2bfd79e70eabcd7826fbdebc1431282
Corey Reid <reid.corey1403@gmail.com>
2024-11-14 11:00:03 -0800

    links split

--- views/links/HomeLinks.tsx
+++ views/links/HomeLinks.tsx
@@ -3,20 +3,36 @@ import { MDBCollapse } from 'mdb-react-ui-kit';
 import PropTypes from 'prop-types';
 import { PhoneLinks, StyledHr } from '../../styles/SharedStyles';
 import LinkButton from './LinkButton';
+import styled from 'styled-components';
 /**
  * Links set up for base links
  */
 function HomeLinks(props: any) {
+
+const Left = styled.div`
+// float: left;
+`;
+const Right = styled.div`
+float: right;
+margin-top: -30px;
+padding-right: 10px
+`;
   return (
     <MDBCollapse open={props.showAnimated}>
       {props.largeScreen ? (
         <>
-          <LinkButton url="/">Home</LinkButton>
-          <LinkButton url="/Wines">Wines</LinkButton>
-          <LinkButton url="/Products">Products</LinkButton>
-          <LinkButton url="/Events">Events</LinkButton>
-          <LinkButton url="/ContactUs">Contact Us</LinkButton>
-          <LinkButton url="/History">History</LinkButton>
+          <Left>
+            <LinkButton url="/">Home</LinkButton>
+            <LinkButton url="/Wines">Wines</LinkButton>
+            <LinkButton url="/Products">Products</LinkButton>
+            <LinkButton url="/Products">1</LinkButton>
+          </Left>
+          <Right>
+            <LinkButton url="/Products">2</LinkButton>
+            <LinkButton url="/Events">Events</LinkButton>
+            <LinkButton url="/ContactUs">Contact Us</LinkButton>
+            <LinkButton url="/History">History</LinkButton>
+          </Right>
         </>
       ) : (
         <>
@@ -25,6 +41,10 @@ function HomeLinks(props: any) {
           <PhoneLinks href="/Wines">Wines</PhoneLinks>
           <StyledHr />
           <PhoneLinks href="/Products">Products</PhoneLinks>
+          <StyledHr />
+          <PhoneLinks href="/Products">1</PhoneLinks>
+          <StyledHr />
+          <PhoneLinks href="/Products">2</PhoneLinks>
           <StyledHr />
           <PhoneLinks href="/Events">Events</PhoneLinks>
           <StyledHr />
