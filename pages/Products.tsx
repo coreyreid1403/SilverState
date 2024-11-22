b9d6d21ab5d16c73ece91d01456a3e2489939b1a
Corey Reid <reid.corey1403@gmail.com>
2024-11-22 11:03:44 -0800

    Update Products.tsx

--- pages/Products.tsx
+++ pages/Products.tsx
@@ -13,49 +13,47 @@ const Products: NextPage = () => {
   const [wine, setWine] = useState<Wine>(Constants.Wines[0]);
   const Wines2 = [1, 2, 3, 4, 5];
 
-    function dropDownUpdate(change: any) {
-      // setWineName(change.target.value);
-      setWine(Constants.Wines.find(w => w.name === change.target.value) ?? Constants.Wines[0]);
+  function dropDownUpdate(change: any) {
+    // setWineName(change.target.value);
+    setWine(Constants.Wines.find(w => w.name === change.target.value) ?? Constants.Wines[0]);
   }
 
   return (
     <StyledContainer center={true}>
       <Title>Products</Title>
-      <MediumSpacing/>
+      <MediumSpacing />
       <Card>
         <Center>
-        <Form.Select aria-label="Default select example" onChange={(change) => dropDownUpdate(change)}>
-          {Constants.Wines.map((wineDropdown: Wine) => {
-            if(wineDropdown?.years.find(year => year.count > 0)){
-              return <option value={wineDropdown.name}>{wineDropdown.name}</option>;
-            }
-          })}
-        </Form.Select>
-            <CardContent>
-              <p>{wine.name}</p>
-            {/* <StyledTable>
-          <MDBTableHead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Year</th>
-              <th scope="col">$</th>
-            </tr>
-          </MDBTableHead>
-          <MDBTableBody>
-            {wine?.years.map((year: WineYear) => {
-              let wineNumber = 0
-              wineNumber = wineNumber + 1;
-              return (
-                <tr key={wineNumber}>
-                  <th scope="row">{wineNumber}</th>
-                  <td>{year.year}</td>
-                  <td>{year.cost}</td>
-                </tr>
-              );
+          <Form.Select aria-label="Default select example" onChange={(change) => dropDownUpdate(change)}>
+            {Constants.Wines.map((wineDropdown: Wine) => {
+              if (wineDropdown?.years.find(year => year.count > 0)) {
+                return <option value={wineDropdown.name}>{wineDropdown.name}</option>;
+              }
             })}
-          </MDBTableBody>
-        </StyledTable> */}
-            </CardContent>
+          </Form.Select>
+          <CardContent>
+            <p>{wine.name}</p>
+            <StyledTable>
+              <MDBTableHead>
+                <tr>
+                  <th scope="col">Year</th>
+                  <th scope="col">$</th>
+                </tr>
+              </MDBTableHead>
+              <MDBTableBody>
+                {wine?.years.map((year: WineYear) => {
+                  let wineNumber = 0
+                  wineNumber = wineNumber + 1;
+                  return (
+                    <tr key={wineNumber}>
+                      <td width='150px'>{year.year}</td>
+                      <td width='150px'>${year.cost}</td>
+                    </tr>
+                  );
+                })}
+              </MDBTableBody>
+            </StyledTable>
+          </CardContent>
         </Center>
       </Card>
     </StyledContainer>
